1379cef5757bd5bff1f56ead2614aac0012a0054
Sebastian Gutierrez <sebastiancruzing@gmail.com>
2024-05-18 21:32:40 -0500

    db connection using a postgresql databse with neon console

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 import express from 'express'
+import postgres from 'postgres'
+import 'dotenv/config'
 const app = express()
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env
+const sql = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: 'require'
+})
+
+;(async () => {
+  const result = await sql`select version()`
+  console.log(`DB connection to ${result[0].version}`)
+})()
+
 
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
-
 
 app.listen(3000, () => {
   console.log('Listening on http://localhost:3000')
